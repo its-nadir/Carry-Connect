10bdb65fdcc2adf7e3e48ab6bcf684238cf64d93
Abdelfateh Nadir Djelti <168342468+its-nadir@users.noreply.github.com>
2025-12-19 14:50:17 +0100

    Update Footer.jsx

--- next-app/app/components/Footer.jsx
+++ next-app/app/components/Footer.jsx
@@ -1,54 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.inner}>
-        {/* LEFT SECTION */}
+        {/* BRANDING SECTION */}
         <div className={styles.brand}>
-          <div className={styles.logoRow}>
-            <i className="fa-regular fa-globe"></i>
+          <Link href="/" className={styles.logoRow}>
+            <Image 
+              src="/favicon.ico" 
+              alt="CarryConnect" 
+              width={24} 
+              height={24}
+            />
             <h3>CarryConnect</h3>
-          </div>
+          </Link>
 
           <p className={styles.brandText}>
             Connecting travelers with senders for global package delivery.
           </p>
-
-          <div className={styles.socials}>
-            <i className="fa-brands fa-facebook"></i>
-            <i className="fa-brands fa-twitter"></i>
-            <i className="fa-brands fa-instagram"></i>
-            <i className="fa-brands fa-linkedin"></i>
-          </div>
-        </div>
-
-        {/* CENTER COLUMNS */}
-        <div className={styles.columns}>
-          <div>
-            <h4>For Travelers</h4>
-            <a href="#">Add a Trip</a>
-            <a href="#">How It Works</a>
-            <a href="#">Earning Potential</a>
-            <a href="#">Insurance & Protection</a>
-          </div>
-
-          <div>
-            <h4>For Senders</h4>
-            <a href="#">Find a Carrier</a>
-            <a href="#">Shipping Guidelines</a>
-            <a href="#">Package Safety</a>
-            <a href="#">Tracking & Updates</a>
-          </div>
-
-          <div>
-            <h4>Company</h4>
-            <a href="#">About Us</a>
-            <a href="#">Trust & Safety</a>
-            <a href="#">Help Center</a>
-            <a href="#">Terms of Service</a>
-            <a href="#">Privacy Policy</a>
-          </div>
         </div>
       </div>
 
